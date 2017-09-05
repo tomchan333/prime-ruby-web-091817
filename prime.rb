aeea86abe3dea2e377ec33fa67a4e415044d17ac
Tom Chan <tomchanthree@gmail.com>
2017-09-05 02:23:14 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+require 'prime'
+
+def prime?(n)
+  if n > 1
+    for d in 2..(n - 1)
+      if (n % d) == 0
+        return false
+
+      end
+    end
+  else
+    return false
+  end
+true
+end
